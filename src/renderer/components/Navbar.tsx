282ee01ef4f975d9dd21d5dede60560788fb5ceb
T03N <jiangt5@rpi.edu>
2026-03-13 16:50:19 -0400

    Mobile styling changes to Navbar and Home landing page.

--- src/renderer/components/Navbar.tsx
+++ src/renderer/components/Navbar.tsx
@@ -7,7 +7,10 @@ import {
     ListItemIcon,
     ListItemText,
     Toolbar,
-    Typography
+    Typography,
+    IconButton,
+    useMediaQuery,
+    useTheme,
 } from "@mui/material";
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
@@ -16,75 +19,113 @@ import { PiDotsThreeOutlineVerticalFill } from "react-icons/pi";
 import { FaHome } from "react-icons/fa";
 import { FaRegClock } from "react-icons/fa";
 import { FaGear } from "react-icons/fa6";
-import { IoMdAlarm, IoMdLogOut } from "react-icons/io";
+import { IoMdAlarm, IoMdLogOut, IoMdMenu } from "react-icons/io";
 import { auth } from "../firebaseConfig";
 import { GrCircleQuestion } from "react-icons/gr";
-
 
 const drawerWidth = 240;
 
 export function Navbar() {
     const navigate = useNavigate();
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+    const [mobileOpen, setMobileOpen] = useState(false);
 
     const handleSignOut = async () => {
         await signOut(auth);
         navigate('/');
     };
-    
-    return (
-        <Drawer
-            variant="permanent"
-            sx={{
-                width: drawerWidth,
-                flexShrink: 0,
-                [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box', backgroundColor: '#ff7300', color: '#F4F3F2' },
-            }}
-        >
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawerContent = (
+        <Box sx={{ overflow: 'auto' }}>
             <Toolbar>
                 <Typography variant="h6" noWrap component="div">
                     RingUs
                 </Typography>
             </Toolbar>
-            <Box sx={{ overflow: 'auto' }}>
-                <List>
-                    <ListItem disablePadding>
-                        <ListItemButton component={NavLink} to="/" sx={{ color: 'inherit' }}>
-                            <ListItemIcon sx={{ color: 'inherit' }}><FaHome /></ListItemIcon>
-                            <ListItemText primary="Home" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component={NavLink} to="/book-list" sx={{ color: 'inherit' }}>
-                            <ListItemText inset primary="Book list" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component={NavLink} to="/about" sx={{ color: 'inherit' }}>
-                            <ListItemIcon sx={{ color: 'inherit' }}><GrCircleQuestion /></ListItemIcon>
-                            <ListItemText primary="About" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component={NavLink} to="/clock" sx={{ color: 'inherit' }}>
-                            <ListItemIcon sx={{ color: 'inherit' }}><FaRegClock /></ListItemIcon>
-                            <ListItemText primary="Clock" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton component={NavLink} to="/settings" sx={{ color: 'inherit' }}>
-                            <ListItemIcon sx={{ color: 'inherit' }}><FaGear /></ListItemIcon>
-                            <ListItemText primary="Settings" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding>
-                        <ListItemButton onClick={handleSignOut} sx={{ color: 'inherit' }}>
-                            <ListItemIcon sx={{ color: 'inherit' }}><IoMdLogOut /></ListItemIcon>
-                            <ListItemText primary="Logout" />
-                        </ListItemButton>
-                    </ListItem>
-                    
-                </List>
-            </Box>
-        </Drawer>
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton component={NavLink} to="/home" onClick={() => isMobile && setMobileOpen(false)} sx={{ color: 'inherit' }}>
+                        <ListItemIcon sx={{ color: 'inherit' }}><FaHome /></ListItemIcon>
+                        <ListItemText primary="Home" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component={NavLink} to="/alarm" onClick={() => isMobile && setMobileOpen(false)} sx={{ color: 'inherit' }}>
+                        <ListItemIcon sx={{ color: 'inherit' }}><IoMdAlarm /></ListItemIcon>
+                        <ListItemText primary="Alarms" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component={NavLink} to="/book-list" onClick={() => isMobile && setMobileOpen(false)} sx={{ color: 'inherit' }}>
+                        <ListItemText inset primary="Book list" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component={NavLink} to="/about" onClick={() => isMobile && setMobileOpen(false)} sx={{ color: 'inherit' }}>
+                        <ListItemIcon sx={{ color: 'inherit' }}><GrCircleQuestion /></ListItemIcon>
+                        <ListItemText primary="About" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component={NavLink} to="/clock" onClick={() => isMobile && setMobileOpen(false)} sx={{ color: 'inherit' }}>
+                        <ListItemIcon sx={{ color: 'inherit' }}><FaRegClock /></ListItemIcon>
+                        <ListItemText primary="Clock" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component={NavLink} to="/settings" onClick={() => isMobile && setMobileOpen(false)} sx={{ color: 'inherit' }}>
+                        <ListItemIcon sx={{ color: 'inherit' }}><FaGear /></ListItemIcon>
+                        <ListItemText primary="Settings" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={handleSignOut} sx={{ color: 'inherit' }}>
+                        <ListItemIcon sx={{ color: 'inherit' }}><IoMdLogOut /></ListItemIcon>
+                        <ListItemText primary="Logout" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+
+    return (
+        <>
+            {isMobile && (
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="start"
+                    onClick={handleDrawerToggle}
+                    sx={{ position: 'fixed', top: 8, left: 8, zIndex: 1300 }}
+                >
+                    <IoMdMenu />
+                </IconButton>
+            )}
+            <Drawer
+                variant={isMobile ? 'temporary' : 'permanent'}
+                open={isMobile ? mobileOpen : true}
+                onClose={handleDrawerToggle}
+                ModalProps={{ keepMounted: true }}
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: { 
+                        width: drawerWidth, 
+                        boxSizing: 'border-box', 
+                        backgroundColor: '#ff7300', 
+                        color: '#F4F3F2',
+                        position: isMobile ? 'relative' : 'fixed',
+                        height: isMobile ? '100%' : '100vh',
+                    },
+                }}
+            >
+                {drawerContent}
+            </Drawer>
+        </>
     );
 }
