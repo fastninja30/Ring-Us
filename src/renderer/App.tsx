73146a829fdcd7f59d04213b25085f633532a211
fastninja30 <spencerrsu@gmail.com>
2026-03-24 17:09:53 -0400

    Fixed exporting mutable 'let' binding warning + format fixes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,7 +8,6 @@ import { FriendsProvider } from './contexts/FriendsContext';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { Navbar } from './components/Navbar';
 import { BookList } from './pages/BookList';
-import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Clock } from './pages/Clock';
 import { Alarm } from './pages/Alarm';
@@ -37,9 +36,7 @@ function ProtectedLayout({ children }: { children: React.ReactNode }) {
   return (
     <ProtectedRoute>
       <Navbar />
-      <div className="main-content">
-        {children}
-      </div>
+      <div className="main-content"> {children} </div>
     </ProtectedRoute>
   );
 }
@@ -49,52 +46,82 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <AuthProvider>
         <FriendsProvider>
-        <Router>
-          <CssBaseline />
-          <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-          <Navbar />
-          <Box
-            component="main"
-            sx={{
-              flexGrow: 1,
-              p: { xs: 2, sm: 3 },
-              mt: { xs: 6, md: 0 },
-              ml: { xs: 0, md: `${drawerWidth}px` },
-              width: { xs: '100%', md: `calc(100% - ${drawerWidth}px)` },
-            }}
-          >
-          <Routes>
-            {/* Public routes */}
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/verify-email" element={<VerifyEmail />} />
+          <Router>
+            <CssBaseline />
+            <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+              <Navbar />
+              <Box
+                component="main"
+                sx={{
+                  flexGrow: 1,
+                  p: { xs: 2, sm: 3 },
+                  mt: { xs: 6, md: 0 },
+                  ml: { xs: 0, md: `${drawerWidth}px` },
+                  width: { xs: '100%', md: `calc(100% - ${drawerWidth}px)` },
+                }}
+              >
+                <Routes>
+                  {/* Public routes */}
+                  <Route path="/" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/verify-email" element={<VerifyEmail />} />
 
-            {/* Protected routes */}
-{/*             <Route path="/home" element={
+                  {/* Protected routes */}
+                  {/*             <Route path="/home" element={
               <ProtectedLayout><Home /></ProtectedLayout>
             } /> */}
-            <Route path="/alarm" element={
-              <ProtectedLayout><Alarm /></ProtectedLayout>
-            } />
-            <Route path="/clock" element={
-              <ProtectedLayout><Clock /></ProtectedLayout>
-            } />
-            <Route path="/book-list" element={
-              <ProtectedLayout><BookList /></ProtectedLayout>
-            } />
-            <Route path="/about" element={
-              <ProtectedLayout><About /></ProtectedLayout>
-            } />
-            <Route path="/settings" element={
-              <ProtectedLayout><Settings /></ProtectedLayout>
-            } />
-            <Route path="/friends" element={
-              <ProtectedLayout><Friends /></ProtectedLayout>
-            } />
-          </Routes>
-          </Box>
-          </Box>
-        </Router>
+                  <Route
+                    path="/alarm"
+                    element={
+                      <ProtectedLayout>
+                        <Alarm />
+                      </ProtectedLayout>
+                    }
+                  />
+                  <Route
+                    path="/clock"
+                    element={
+                      <ProtectedLayout>
+                        <Clock />
+                      </ProtectedLayout>
+                    }
+                  />
+                  <Route
+                    path="/book-list"
+                    element={
+                      <ProtectedLayout>
+                        <BookList />
+                      </ProtectedLayout>
+                    }
+                  />
+                  <Route
+                    path="/about"
+                    element={
+                      <ProtectedLayout>
+                        <About />
+                      </ProtectedLayout>
+                    }
+                  />
+                  <Route
+                    path="/settings"
+                    element={
+                      <ProtectedLayout>
+                        <Settings />
+                      </ProtectedLayout>
+                    }
+                  />
+                  <Route
+                    path="/friends"
+                    element={
+                      <ProtectedLayout>
+                        <Friends />
+                      </ProtectedLayout>
+                    }
+                  />
+                </Routes>
+              </Box>
+            </Box>
+          </Router>
         </FriendsProvider>
       </AuthProvider>
     </ThemeProvider>
